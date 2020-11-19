8414160e19fbf490883b2c4cf76c78b93d2690ea
Kim Taewon <teawub@naver.com>
2020-11-19 14:41:42 +0900

    Riot API ImageURL

--- app/src/main/java/com/taewon/retrofittt/MainActivity.kt
+++ app/src/main/java/com/taewon/retrofittt/MainActivity.kt
@@ -3,24 +3,39 @@ package com.taewon.retrofittt
 import android.app.Activity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : Activity() {
-    val API_KEY: String = "RGAPI-9ec7c329-788a-45c8-89a0-4defc9e54b36"
+    val API_KEY: String = "RGAPI-a35269d5-4f37-490c-b96e-9f8ad33428f2"
+    var IconURL: String =""
+    var AcName: String? =""
+    var AcLevel: String? =""
+    var AcIcon: String? =""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var imageView: ImageView = findViewById(R.id.icon)
 
-        val response = SearchRetrofit.getService().requestSearchRiot("태원이심신미약",API_KEY)
+        val response = SearchRetrofit.getService().requestSearchAccount("용정이 두둥등장",API_KEY)
         response.enqueue(object : Callback<Summoner>{
             override fun onResponse(call: Call<Summoner>, response: Response<Summoner>) {
-                response?.let {
+                /*response?.let {
                     var asd: Summoner? = it.body()
 
                     Log.i("여기",asd.toString())
-                }
+                }*/
+                var asd: Summoner? = response.body()
+                AcName = asd?.name
+                AcLevel = asd?.summonerLevel
+                AcIcon = asd?.profileIconId
+
+                Log.i("여기",asd.toString())
+                IconURL = "http://ddragon.leagueoflegends.com/cdn/10.23.1/img/profileicon/${AcIcon}.png"
+                imageView.loadUrl(IconURL)
             }
 
             override fun onFailure(call: Call<Summoner>, t: Throwable) {
@@ -28,5 +43,8 @@ class MainActivity : Activity() {
             }
         })
 
+    }
+    fun ImageView.loadUrl(url: String){
+        Picasso.with(context).load(url).into(this)
     }
 }
--- app/src/main/java/com/taewon/retrofittt/RiotAPI.kt
+++ app/src/main/java/com/taewon/retrofittt/RiotAPI.kt
@@ -1,5 +1,6 @@
 package com.taewon.retrofittt
 
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -7,8 +8,12 @@ import retrofit2.http.Query
 
 interface RiotAPI {
     @GET("/lol/summoner/v4/summoners/by-name/{id}")
-    fun requestSearchRiot(
+    fun requestSearchAccount(
             @Path("id") id: String,
             @Query("api_key") api_key: String
     ): Call<Summoner>
+    @GET("http://ddragon.leagueoflegends.com/cdn/10.23.1/img/item/{imageId}.png")
+    fun requestSearchItem(
+            @Path("imageId") id: String
+    ): Call<ResponseBody>
 }
